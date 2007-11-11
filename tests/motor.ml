406e77db351a9738183f5529b9681af8b707fb58
chris_77 <>
2007-11-11 22:31:58 +0000

    - Improved Motor.set and added a motor test.

---
+++ tests/motor.ml
@@ -0,0 +1,33 @@
+open Printf
+
+module Motor = Mindstorm.Motor
+
+let bt = ref ""
+let speed = ref 30
+let rot_deg = ref(5 * 360)
+
+let () =
+  let args = Arg.align [
+    "-speed", Arg.Set_int speed, "s Set the speed (in -100 .. 100)";
+    "-s", Arg.Set_int speed, "s Set the speed (in -100 .. 100)";
+    "-rotate", Arg.Set_int rot_deg, "r Rotation in degrees";
+    "-r", Arg.Set_int rot_deg, "r Rotation in degrees";
+  ]
+  and usage_msg = sprintf "%s <options> bluetooth_addr" Sys.argv.(0) in
+  Arg.parse args (fun addr -> bt := addr) usage_msg;
+  if !bt = "" then begin Arg.usage args usage_msg; exit 1 end
+
+
+let () =
+  let conn = Mindstorm.connect_bluetooth !bt in
+  printf "Rotate motor connected to port A by %i degrees... %!" !rot_deg;
+  let st = { Motor.speed = !speed;
+             mode = [`Motor_on];
+             turn_ratio = 0;
+             run_state = `Ramp_up;
+             tach_limit = !rot_deg } in
+  Motor.set conn Motor.a st;
+  printf "done\n%!";
+  Unix.sleep 3; (* we cannot exit immediately, otherwise no motor
+                   rotation is performed. *)
+  Mindstorm.close conn
