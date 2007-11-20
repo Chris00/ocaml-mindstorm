d1aba41ea6eaa21c8d7ec0dc1950fcd36555a0f6
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2007-11-20 18:19:59 +0000

    - Corrected the documentation of roam.ml - Introduced a counter to see requests being made.

--- examples/roam.ml
+++ examples/roam.ml
@@ -1,11 +1,10 @@
 open Printf
 module Sensor = Mindstorm.Sensor
-module U = Mindstorm.Sensor.Ultrasonic
 module Motor = Mindstorm.Motor
 
 (* `S1 : touch sensor
    `S4 : ultrasonic sensor
-   Motors connected to A and B. *)
+   Motors connected to B and C (like the basic robot of the LEGO® set). *)
 
 let usleep s = ignore(Unix.select [] [] [] s)
 
@@ -16,12 +15,14 @@ let run conn =
 
   Sensor.set conn `S1 `Switch `Bool;
   let ultra = Sensor.Ultrasonic.make conn `S4 in
+  let cnt = ref 0 in
   while true do
+    incr cnt;
     let switch = Sensor.get conn `S1 in
     if switch.Sensor.scaled = 0 then begin
       Sensor.Ultrasonic.set ultra `Meas;
       let dist = min 50 (Sensor.Ultrasonic.get ultra `Byte0) in
-      printf "dist = %i\r%!" dist;
+      printf "%4i: dist = %i\r%!" !cnt dist;
       speed dist dist;  (*(2 * dist - 50)*)
       usleep 0.2;
     end
