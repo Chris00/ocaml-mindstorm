95228300601423faeaca52f197f23b9e9dc729b1
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2008-03-08 20:06:54 +0000

    - Merged battery.ml into info.ml and removed the former. - Made the brick name change optional in info.ml. - Adapted the Makefile to use the automatic platform detection.

--- tests/info.ml
+++ tests/info.ml
@@ -1,16 +1,25 @@
 open Printf
 
-let bt =
-  if Array.length Sys.argv < 2 then (
-    printf "%s <bluetooth addr>\n" Sys.argv.(0);
-    exit 1;
-  )
-  else Sys.argv.(1)
+let new_brick_name = ref None
+let bt = ref ""
+
+let args = Arg.align [
+  "--name", Arg.String(fun n -> new_brick_name := Some n),
+  "n Set a new name for the brick"
+]
+let usage_msg = sprintf "%s <bluetooth addr>" Sys.argv.(0)
+
 
 let () =
-  let conn = Mindstorm.connect_bluetooth bt in
+  Arg.parse args (fun a -> bt := a) usage_msg;
+  let conn =
+    if !bt <> "" then Mindstorm.connect_bluetooth !bt
+    else (Arg.usage args usage_msg; exit 0) in
   printf "Connected!\n%!";
-  Mindstorm.set_brick_name conn "OCaml" ~check_status:true;
+  begin match !new_brick_name with
+  | None -> ()
+  | Some name -> Mindstorm.set_brick_name conn name ~check_status:true;
+  end;
   printf "Device info: \n%!";
   let i = Mindstorm.get_device_info conn in
   printf "- brick name = %S\n" i.Mindstorm.brick_name;
@@ -19,4 +28,7 @@ let () =
   printf "- free user FLASH = %i bytes\n" i.Mindstorm.free_user_flash;
   let (p1, p0, f1, f0) = Mindstorm.firmware_version conn in
   printf "- protocol = %i.%i, firmware = %i.%02i\n" p1 p0 f1 f0;
+  printf "Battery level: %!";
+  let bat = Mindstorm.battery_level conn in
+  printf "%i millivolts\n%!" bat;
   Mindstorm.close conn
