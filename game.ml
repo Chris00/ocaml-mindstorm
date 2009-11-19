082f03bb9d5cae21aff3ee84f2822d13bfc80c34
Vinciane De Waele (vincianedewaele@hotmail.com) <>
2009-11-19 14:59:40 +0100

    ds game: réécriture de la méthode move pour mettre à jour les lignes de pions en cours ds plateauJeu: inclusion des méthodes de game pour pouvoir jouer ac l'interface graphique

--- game.ml
+++ game.ml
@@ -1,4 +1,9 @@
 type piece = Red | Yellow | Empty
+type couple_current_piece =
+    {
+      mutable current_piece : piece;
+      mutable tab_line_piece : int array
+    }
 
 (*un evenement c'est l'endroit ou l'on place le pion et la couleur de celui-ci*)
 (*attention raw et line démarre a 1 et dans les algos a 0*)
@@ -10,16 +15,18 @@ type event =
     }
 
 (*un jeu c'est un tableau représentant l'état du jeu
+  un autre tableau représentant le pion et les lignes dont il fait parti
   et une liste des événements placés du plus recents au plus vieux*)
 type g =
     {
       mutable tab : piece array array;
+      mutable tab_line : couple_current_piece array array; (*on aurait pu le mettre ac tab mais c pour calculer si on a gagné sans l'arbre qu'on n'a pas encore implémenter*) (*le tab intérieur et de taille 4, représente les 4 lignes : |, -, /, \ *)
       mutable list_event : event list
     }
-
+ 
 (*création d'un jeu -> on initialise le tableau a non et la liste d'événement
   est vide*)
-let make () = { tab = Array.init 7 (fun i -> (Array.make 6 Empty)); list_event = [] };;
+let make () = { tab = Array.init 7 (fun i -> (Array.make 6 Empty)); tab_line = Array.init 7 (fun i -> Array.init 6 (fun i -> {current_piece = Empty; tab_line_piece = Array.make 4 0})); list_event = [] };;
 
 (*récupération de la couleur (s'il y a ) du pion en i,j*)
 let get_piece current_game j i = current_game.tab.(j).(i);;
@@ -33,11 +40,48 @@ let move current_game j p =
   if (!i<>n-1) then
     (
       current_game.tab.(j-1).(!i) <- p;
-      current_game.list_event <- [{raw = j; line = !i+1; piece = p}]@current_game.list_event
+      current_game.tab_line.(j-1).(!i).current_piece <- p;
+      (*on remplit le tab ac les lignes dont p fait parti et on modifie les tab des pions de ces lignes*)
+      (* pour la ligne verticale *)
+      if (!i != 0 && current_game.tab_line.(j-1).(!i-1).current_piece = p)
+      then
+        (
+          let piece_in_line = current_game.tab_line.(j-1).(!i-1).tab_line_piece.(0) + 1 in
+          current_game.tab_line.(j-1).(!i).tab_line_piece.(0) <- piece_in_line;
+          for k = 1 to (piece_in_line - 1) do
+            current_game.tab_line.(j-1).(!i-k).tab_line_piece.(0) <- piece_in_line;
+          done;
+        )
+      else current_game.tab_line.(j-1).(!i).tab_line_piece.(0) <- 1;
+
+      (*pour la ligne horizontale*)
+      let g = ref 0 and d = ref 0 in
+
+      if (j-1 > 0 && current_game.tab_line.(j-2).(!i).current_piece = p) then (g := current_game.tab_line.(j-2).(!i).tab_line_piece.(1));
+
+      if (j-1 < 6 && current_game.tab_line.(j).(!i).current_piece = p) then  d := current_game.tab_line.(j).(!i).tab_line_piece.(1);
+
+      current_game.tab_line.(j-1).(!i).tab_line_piece.(1) <- !g + 1 + !d;
+
+      if (!g != 0) then
+        (
+          for k = 1 to !g do current_game.tab_line.(j-k-1).(!i).tab_line_piece.(1) <- !g + 1 + !d done;
+        );
+
+      if (!d != 0) then
+        (
+          for k = 1 to !d do current_game.tab_line.(j-1+k).(!i).tab_line_piece.(1) <- !g + 1 + !d done;
+        );
+      
+      (* current_game.tab_line.(j-1).(!i) <- ;
+         current_game.tab_line.(j-1).(!i) <- ;*)(*plus modifier les voisins*)
+      current_game.list_event <-
+        [{raw = j; line = !i+1; piece = p}]@current_game.list_event
     );
   current_game;
 ;;
 
+(*retourne de num coup en arrière dans le jeu*)
 let rec remove current_game num =
   if (num<>0)then
     (
@@ -45,20 +89,31 @@ let rec remove current_game num =
       let j = next_event.raw
       and i = next_event.line in
       current_game.tab.(j-1).(i-1) <- Empty;
+      current_game.tab_line.(j-1).(i-1) <- {current_piece = Empty; tab_line_piece = [|0;0;0;0|]}; (*plus changer les valeurs des voisins...*)
       current_game.list_event <- List.tl current_game.list_event;
       remove current_game (num-1)
     )
   else current_game
 ;;
 
+(*création d'une nouvelle partie*)
 let new_part current_game =
   current_game.tab <- Array.init 7 (fun i -> (Array.make 6 Empty));
+  current_game.tab_line <- Array.init 7 (fun i -> Array.make 6 ({current_piece = Empty; tab_line_piece = [|0;0;0;0|]}));
   current_game.list_event <- [];
   current_game;;
+
+
+
+
 
 (*TEST*)
 let game_test = make();;
 move game_test 4 Red;;
+move game_test 4 Red;;
+move game_test 4 Red;;
+move game_test 6 Red;;
+move game_test 5 Red;;
 move game_test 4 Yellow;;
 remove game_test 1;;
 new_part game_test;;
--- plateauJeu.ml
+++ plateauJeu.ml
@@ -1,8 +1,22 @@
 open Graphics
 open Printf
+open Game
 
+let w = 1000 and h = 720
 
-let gameboard w h =
+(*methode qui colorie les cercles dans la couleur color*)
+let color_circle color x_center y_center =
+  let r_circle = 7*(min (w/9) (h/9))/16 in
+  set_color color;
+  fill_circle x_center y_center r_circle;
+  set_color black;
+  draw_circle x_center y_center r_circle;;
+
+(*methode qui trace un disque dans la couleur color, en position (raw, line)*)
+let color_circle2 color raw line =
+  color_circle color (w/18 + raw*w/9) (h/9 + h/18 + line*h/9);;
+
+let gameboard current_game =
   open_graph(sprintf " %ix%i" w h );
   set_window_title("Connect Four");
 
@@ -30,71 +44,95 @@ let gameboard w h =
   let worh = min (w/9) (h/9) in
   let r_circle = 7*worh/16 in
 
-  for i=0 to 6 do
-    for j=0 to 5 do
-      set_color white;
-      let x_circle = w/6+i*w/9 and y_circle = 5*h/18+j*h/9 in
-      fill_circle x_circle y_circle r_circle;
-      set_color black;
-      draw_circle x_circle y_circle r_circle;
-    done;
-  done;
+  if (List.length current_game.list_event = 0) then
+    for i=0 to 6 do
+      for j=0 to 5 do
+        set_color white;
+        let x_circle = w/6+i*w/9 and y_circle = 5*h/18+j*h/9 in
+        fill_circle x_circle y_circle r_circle;
+        set_color black;
+        draw_circle x_circle y_circle r_circle;
+      done;
+    done
 
-
-
-  (*utilisation bouton New Part*)
-  let clic = wait_next_event [Button_down; Button_up] in
-  if button_down() then
-    (
-      let pos_x = clic.mouse_x and pos_y = clic.mouse_y in
-      if ( (pos_x>(x-8)) && (pos_x<(x+n_x+6))
-           && (pos_y>(y-7)) && (pos_y<(y+n_y+7)) )
-      then
-        (
-          set_color white;
-          draw_rect (x-4) (y-3) (n_x+8) (n_y+6);
-        )
-    )
   else
-    (
-      let pos_x = clic.mouse_x and pos_y = clic.mouse_y in
-      if ( (pos_x>(x-8)) && (pos_x<(x+n_x+6))
-           && (pos_y>(y-7)) && (pos_y<(y+n_y+7)) )
-      then
-        (
-          set_color black;
-          set_line_width 1;
-          draw_rect (x-4) (y-3) (n_x+8) (n_y+6);
-          set_line_width 2;
-          for i=0 to 6 do
-            for j=0 to 5 do
-              set_color white;
-              let x_circle = w/6+i*w/9 and y_circle = 5*h/18+j*h/9 in
-              fill_circle x_circle y_circle r_circle;
-              set_color black;
-              draw_circle x_circle y_circle r_circle;
+    for i=0 to 6 do
+      for j=0 to 5 do
+        if (current_game.tab.(i).(j) = Empty) then set_color white
+        else if (current_game.tab.(i).(j) = Red) then set_color red
+        else set_color yellow;
+
+        let x_circle = w/6+i*w/9 and y_circle = 5*h/18+j*h/9 in
+        fill_circle x_circle y_circle r_circle;
+        set_color black;
+        draw_circle x_circle y_circle r_circle;
+      done;
+    done;
+
+  (*utilisation bouton New Part et du quadrillage*)
+  let rec part player1 player2 =
+    let clic = wait_next_event [Button_down; Button_up] in
+    if button_down() then
+      (
+        let pos_x = clic.mouse_x and pos_y = clic.mouse_y in
+        if ( (pos_x>(x-8)) && (pos_x<(x+n_x+6))
+             && (pos_y>(y-7)) && (pos_y<(y+n_y+7)) )
+        then
+          (
+            set_color white;
+            draw_rect (x-4) (y-3) (n_x+8) (n_y+6);
+            part player1 player2
+          )
+        else if ((pos_x>(w/9)) && (pos_x<(8*w/9)) &&
+                   (pos_y>(2*h/9)) && (pos_y<(8*h/9)))
+        then
+          (
+            let raw = pos_x/(w/9) in
+            if (current_game.tab.(raw-1).(5) = Empty) then
+              (
+                move current_game raw player1;
+                let action = List.hd current_game.list_event in
+                if (player1 = Red) then
+                  color_circle2 red (action.raw) (action.line)
+                else color_circle2 yellow (action.raw) (action.line);
+                part player2 player1;
+              )
+            else part player1 player2;
+          );
+      )
+    else
+      (
+        let pos_x = clic.mouse_x and pos_y = clic.mouse_y in
+        if ( (pos_x>(x-8)) && (pos_x<(x+n_x+6))
+             && (pos_y>(y-7)) && (pos_y<(y+n_y+7)) )
+        then
+          (
+            set_color black;
+            set_line_width 1;
+            draw_rect (x-4) (y-3) (n_x+8) (n_y+6);
+            set_line_width 2;
+            for i=0 to 6 do
+              for j=0 to 5 do
+                set_color white;
+                let x_circle = w/6+i*w/9 and y_circle = 5*h/18+j*h/9 in
+                fill_circle x_circle y_circle r_circle;
+                set_color black;
+                draw_circle x_circle y_circle r_circle;
+              done;
             done;
-          done;
-        )
-    )
+            new_part current_game;
+            part player1 player2;
+          );
+        part player1 player2;
+      );
+    part player1 player2 in
+  part Red Yellow
 ;;
-let onBoutonNP pos_x pos_y =
-  ((pos_x>(x-8)) && (pos_x<(x+n_x+6))
-   && (pos_y>(y-7)) && (pos_y<(y+n_y+7)));;
 
-(*methode qui colorie les cercles dans la couleur color*)
-let color_circle color x_center y_center w h=
-  let r_circle = 7*(min (w/9) (h/9))/16 in
-  set_color color;
-  fill_circle x_center y_center r_circle;
-  set_color black;
-  draw_circle x_center y_center r_circle;;
 
-(*methode qui trace un disque dans la couleur color, en position (raw, line)*)
-let color_circle2 color raw line w h=
-  color_circle color (w/6 + raw*w/9) (5*h/18 + line*h/9);;
 
 
 
 (*let st = wait_next_event [Button_down] in ();;*)
-(*gameboard 1000 720;;*)
+let game_test = make() in
+gameboard game_test;;
