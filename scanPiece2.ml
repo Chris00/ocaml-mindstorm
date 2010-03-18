8bbcc1cfd6c4e864f40bea583a37ad423a6dd1f7
Vinciane De Waele (vincianedewaele@hotmail.com) <>
2010-03-18 19:18:45 +0100

    reste à finir de remplir les tableaux des vitesses

--- scanPiece2.ml
+++ scanPiece2.ml
@@ -15,7 +15,7 @@ let next_col = ref (-1)
 let light = ref true (*mettre à faux lorsqu'on veut juste remettre le capteur
  à droite*)
 let go_to_next = ref false
-let current_game = ref 1635422 (*representation du jeu par un entier*)
+let current_game = ref 0 (*representation du jeu par un entier*)
 (*num du jeu: de 0 a 6 de gauche à droit du cote du joueur*)
 let expo_10 = [| 1; 10; 100; 1000; 10000; 100000; 1000000|]
 
@@ -26,10 +26,10 @@ let adjust_r = 27
 let adjust_v = 20
 
 
-let shift_h = 198
-let shift_up_v = 132
-let shift_up_r = 189
-let shift_up_l = 189
+let shift_h = 204
+let shift_up_v = 131
+let shift_up_r = 184
+let shift_up_l = 184
 
 (*angle de rotation de moteur_captor_vert pr la ligne [l]*)
 let rot_v l =
@@ -46,40 +46,40 @@ let rot_l l c =
 
 
 (*tab des vitesses des trois moteurs (vert, r, l) lors des déplacements vers la
-  gauche. pr aller à droite vitesse r = -vitesse l*)
+  gauche. pr aller à droite vitesse, le tableau nous donne (vert, l, r)*)
 (*speed_up.(i).(j) nous donne les vitesses des moteurs (vert, r, l) lorsqu'on
   doit se déplacer de i lignes vers le haut et de j col vers la gauche*)
 let speed_up =
   [|
-    [|(0, 0, 0); (0, 13, -12); (0, 13, -12); (0, 13, -12);
-      (0, 13, -12); (0, 13, -12); (0, 13, -12)|];
-    [|(10, 20, 20); (0, 0, 0); (0, 0, 0); (0, 0, 0);
+    [|(0, 0, 0); (0, 15, -14); (0, 15, -14); (0, 15, -14);
+      (0, 15, -14); (0, 15, -14); (0, 15, -14)|];
+    [|(11, 18, 18); (8, 22, -2); (6, 23, -10); (0, 0, 0);
       (0, 0, 0); (0, 0, 0); (0, 0, 0)|];
-    [|(10, 20, 20); (0, 0, 0); (0, 0, 0); (0, 0, 0);
+    [|(11, 18, 18); (0, 0, 0); (8, 22, -2); (0, 0, 0);
+      (5, 24, -10); (0, 0, 0); (0, 0, 0)|];
+    [|(11, 17, 17); (0, 0, 0); (0, 0, 0); (8, 23, -2);
       (0, 0, 0); (0, 0, 0); (0, 0, 0)|];
-    [|(10, 20, 20); (0, 0, 0); (0, 0, 0); (0, 0, 0);
-      (0, 0, 0); (0, 0, 0); (0, 0, 0)|];
-    [|(10, 20, 20); (0, 0, 0); (0, 0, 0); (0, 0, 0);
-      (0, 0, 0); (0, 0, 0); (0, 0, 0)|];
-    [|(10, 20, 20); (0, 0, 0); (0, 0, 0); (0, 0, 0);
-      (0, 0, 0); (0, 0, 0); (0, 0, 0)|]
+    [|(11, 17, 17); (0, 0, 0); (0, 0, 0); (0, 0, 0);
+      (7, 23, -2); (0, 0, 0); (0, 0, 0)|];
+    [|(11, 17, 17); (9, 15, 13); (0, 0, 0); (0, 0, 0);
+      (0, 0, 0); (7, 22, -2); (0, 0, 0)|]
   |]
 
 (*meme chose mais vers le bas*)
 let speed_down =
   [|
-    [|(0, 0, 0); (0, 13, -12); (0, 13, -12); (0, 13, -12);
-      (0, 13, -12); (0, 13, -12); (0, 13, -12)|];
-    [|(-10, -13, -13); (0, 0, 0); (0, 0, 0); (0, 0, 0);
+    [|(0, 0, 0); (0, 15, -14); (0, 15, -14); (0, 15, -14);
+      (0, 15, -14); (0, 15, -14); (0, 15, -14)|];
+    [|(-10, -13, -13); (-9, 2, -22); (-7, 7, -19); (0, 0, 0);
+      (0, 0, 0); (0, 0, 0); (0, 0, 0)|];
+    [|(-10, -13, -13); (0, 0, 0); (-9, 2, -22); (0, 0, 0);
+      (-6, 6, -17); (0, 0, 0); (0, 0, 0)|];
+    [|(-10, -13, -13); (0, 0, 0); (0, 0, 0); (-9, 1, -22);
       (0, 0, 0); (0, 0, 0); (0, 0, 0)|];
     [|(-10, -13, -13); (0, 0, 0); (0, 0, 0); (0, 0, 0);
-      (0, 0, 0); (0, 0, 0); (0, 0, 0)|];
-    [|(-10, -13, -13); (0, 0, 0); (0, 0, 0); (0, 0, 0);
-      (0, 0, 0); (0, 0, 0); (0, 0, 0)|];
-    [|(-10, -13, -13); (0, 0, 0); (0, 0, 0); (0, 0, 0);
-      (0, 0, 0); (0, 0, 0); (0, 0, 0)|];
-    [|(-10, -13, -13); (0, 0, 0); (0, 0, 0); (0, 0, 0);
-      (0, 0, 0); (0, 0, 0); (0, 0, 0)|]
+      (-8, 1, -24); (0, 0, 0); (0, 0, 0)|];
+    [|(-10, -13, -13); (-9, -11, -15); (0, 0, 0); (0, 0, 0);
+      (0, 0, 0); (-8, 1, -25); (0, 0, 0)|]
   |]
 
 
@@ -279,61 +279,68 @@ struct
 
 
 
+
+
+
   (*attend d'avoir fini de monter (avec ralenti) avant de scanner la case*)
-  let wait_up_end angle_v f _ =
-    let st = {speed = 0; motor_on = true; brake = true;
-              regulation = `Idle; turn_ratio = 100;
-              run_state = `Ramp_down; tach_limit = 30} in
-    Motor.set C.conn motor_captor_vert st;
-    Robot.event meas_vert (function
-                           |None -> false
-                           |Some d -> d >= angle_v)
-      (scan_light f)
+  (* let wait_up_end angle_v f _ = *)
+  (*     let st = {speed = 0; motor_on = true; brake = true; *)
+  (*               regulation = `Idle; turn_ratio = 100; *)
+  (*               run_state = `Ramp_down; tach_limit = 30} in *)
+  (*     Motor.set C.conn motor_captor_vert st; *)
+  (*     Robot.event meas_vert (function *)
+  (*                            |None -> false *)
+  (*                            |Some d -> d >= angle_v) *)
+  (*       (scan_light f) *)
 
   let wait_up angle_v f =
     Robot.event meas_vert (function
                            |None -> false
-                           |Some d -> d >= angle_v - 20)
-      (wait_up_end angle_v f)
+                           |Some d -> d >= angle_v)
+      (* (wait_up_end angle_v f) *)
+      (scan_light f)
+
 
   (*attend d'avoir fini ac le moteur r (ac ralenti) avant de scanner la case*)
-  let wait_down_right_end angle_r f _ =
-    let st = {speed = 0; motor_on = true; brake = true;
-              regulation = `Idle; turn_ratio = 100;
-              run_state = `Ramp_down; tach_limit = 30} in
-    Motor.set C.conn motor_captor_r st;
-    Robot.event meas_right (function
-                           |None -> false
-                           |Some d -> d <= angle_r)
-      (scan_light f)
+  (*  let wait_down_right_end angle_r f _ = *)
+  (*     let st = {speed = 0; motor_on = true; brake = true; *)
+  (*               regulation = `Idle; turn_ratio = 100; *)
+  (*               run_state = `Ramp_down; tach_limit = 30} in *)
+  (*     Motor.set C.conn motor_captor_r st; *)
+  (*     Robot.event meas_right (function *)
+  (*                            |None -> false *)
+  (*                            |Some d -> d <= angle_r) *)
+  (*       (scan_light f) *)
 
   let wait_down_right angle_r f =
     Robot.event meas_right (function
                            |None -> false
-                           |Some d -> d <= angle_r + 20)
-      (wait_down_right_end angle_r f)
+                           |Some d -> d <= angle_r)
+      (* (wait_down_right_end angle_r f) *)
+      (scan_light f)
+
 
   (*attend d'avoir fini ac le moteur l (ac ralenti) avant de scanner la case*)
-  let wait_down_left_end angle_l f _ =
-    let st = {speed = 0; motor_on = true; brake = true;
-              regulation = `Idle; turn_ratio = 100;
-              run_state = `Ramp_down; tach_limit = 30} in
-    Motor.set C.conn motor_captor_l st;
-    Robot.event meas_right (function
-                           |None -> false
-                           |Some d -> d <= angle_l)
-      (scan_light f)
+  (* let wait_down_left_end angle_l f _ = *)
+  (*     let st = {speed = 0; motor_on = true; brake = true; *)
+  (*               regulation = `Idle; turn_ratio = 100; *)
+  (*               run_state = `Ramp_down; tach_limit = 40} in *)
+  (*     Motor.set C.conn motor_captor_l st; *)
+  (*     Robot.event meas_right (function *)
+  (*                            |None -> false *)
+  (*                            |Some d -> d <= angle_l+5) *)
+  (*       (scan_light f) *)
 
 
   let wait_down_left angle_l f =
     Robot.event meas_left (function
                            |None -> false
-                           |Some d -> d <= angle_l + 20)
-      (wait_down_left_end angle_l f)
+                           |Some d -> d <= angle_l)
+      (* (wait_down_left_end angle_l f) *)
+      (scan_light f)
 
 
-
-  let scan_case new_pos_line new_pos_col f =
+  let rec scan_case new_pos_line new_pos_col f =
     let diff_line = new_pos_line - !current_line and
         diff_col = new_pos_col - !current_col and
         angle_v = rot_v new_pos_line and
@@ -343,97 +350,114 @@ struct
     current_line := new_pos_line;
     current_col := new_pos_col;
 
-
-    (*on fait les deux mouvements en meme temps*)
-    if (diff_line >= 0) then (*on va vers le haut*)
+    if(diff_line = 0 && diff_col = 0) then
+      (adjustment new_pos_line new_pos_col  f)
+    else
       (
-        (match (Robot.read meas_vert) with
-         |Some m  ->
-            Motor.set C.conn motor_captor_vert (Motor.speed ~tach_limit:
-                     (angle_v - m + 5)
-                     (fst speed_up.(diff_line).(diff_col)))
-         |None -> assert false);
-
-
-        if (diff_col >= 0) then (*on va vers la gauche*)
+        (*on fait les deux mouvements en meme temps*)
+        if (diff_line > 0) then (*on va vers le haut*)
           (
-            (match (Robot.read meas_right) with
-             |Some m  ->
-                Motor.set C.conn motor_captor_r (Motor.speed ~tach_limit:
-                      (angle_r - m) (scd speed_up.(diff_line).(diff_col)))
+            printf"monte\n%!";
+            (match (Robot.read meas_vert) with
+             |Some m_v  ->
+                (
+                  Motor.set C.conn motor_captor_vert (Motor.speed
+                        (fst speed_up.(diff_line).(diff_col)));
+                  wait_up angle_v f
+                )
              |None -> assert false);
 
-            (match (Robot.read meas_left) with
-             |Some m  ->
-                Motor.set C.conn motor_captor_l (Motor.speed ~tach_limit:
-                      (abs(angle_l - m)) (lst speed_up.(diff_line).(diff_col)))
-             |None -> assert false)
+
+            if (diff_col >= 0) then (*on va vers la gauche*)
+              (
+                printf"va à gauche\n%!";
+                (match (Robot.read meas_right) with
+                 |Some m_r  ->
+                    Motor.set C.conn motor_captor_r (Motor.speed
+                      ~tach_limit: (abs(angle_r - m_r))
+                      (scd speed_up.(diff_line).(diff_col)))
+                 |None -> assert false);
+
+                (match (Robot.read meas_left) with
+                 |Some m_l  ->
+                    Motor.set C.conn motor_captor_l (Motor.speed
+                      ~tach_limit: (abs(angle_l - m_l))
+                      (lst speed_up.(diff_line).(diff_col)))
+                 |None -> assert false)
+              )
+            else (*on va vers la droite*)
+              (
+                printf"va à droite\n%!";
+                (match (Robot.read meas_right) with
+                 |Some m_r  ->
+                    Motor.set C.conn motor_captor_r (Motor.speed
+                       ~tach_limit: (abs(angle_r - m_r))
+                       (lst speed_up.(diff_line).(-diff_col)))
+                 |None -> assert false);
+
+
+                (match (Robot.read meas_left) with
+                 |Some m_l  ->
+                    Motor.set C.conn motor_captor_l (Motor.speed
+                      ~tach_limit: (abs(angle_l - m_l))
+                      (scd speed_up.(diff_line).(-diff_col)))
+                 |None -> assert false)
+              )
           )
-        else (*on va vers la droite*)
+
+        else (*on descend*)
           (
-            (match (Robot.read meas_right) with
-             |Some m  ->
-                Motor.set C.conn motor_captor_r (Motor.speed
-                 ~tach_limit: (abs(angle_r - m))
-                 ((-1)*(lst speed_up.(diff_line).(abs(diff_col)))))
+            printf"descend\n%!";
+            (match (Robot.read meas_vert) with
+             |Some m_v  ->
+                Motor.set C.conn motor_captor_vert (Motor.speed
+                    ~tach_limit: (abs(m_v -angle_v))
+                    (fst speed_down.(-diff_line).(abs(diff_col))))
              |None -> assert false);
 
 
-            (match (Robot.read meas_left) with
-             |Some m  ->
-                Motor.set C.conn motor_captor_l (Motor.speed
-                      ~tach_limit: (angle_l - m)
-                      ((-1)*(scd speed_up.(diff_line).(abs(diff_col)))))
-             |None -> assert false)
-          );
-        wait_up angle_v f
+            if (diff_col >= 0) then (*on va vers la gauche*)
+              (
+                printf"va à gauche\n%!";
+
+                (match (Robot.read meas_right) with
+                 |Some m_r  ->
+                    Motor.set C.conn motor_captor_r (Motor.speed
+                          ~tach_limit: (abs(angle_r - m_r))
+                          (scd speed_down.(-diff_line).(diff_col)))
+                 |None -> assert false);
+
+                (match (Robot.read meas_left) with
+                 |Some m_l  ->
+                    (
+                      Motor.set C.conn motor_captor_l (Motor.speed
+                         (lst speed_down.(-diff_line).(diff_col)));
+                      wait_down_left angle_l f
+                    )
+                 |None -> assert false);
+
+              )
+            else (*on va vers la droite*)
+              (
+                printf"va à droite\n%!";
+                (match (Robot.read meas_right) with
+                 |Some m_r  ->
+                    (
+                      Motor.set C.conn motor_captor_r (Motor.speed
+                             (lst speed_down.(-diff_line).(-diff_col)));
+                      wait_down_right angle_r f
+                    )
+                 |None -> assert false);
+
+                (match (Robot.read meas_left) with
+                 |Some m_l  ->
+                    Motor.set C.conn motor_captor_l (Motor.speed
+                      ~tach_limit: (abs(angle_l - m_l))
+                      (scd speed_down.(-diff_line).(-diff_col)))
+                 |None -> assert false);
+              )
+          )
       )
-
-    else (*on descend*)
-      (
-        (match (Robot.read meas_vert) with
-         |Some m  ->
-            Motor.set C.conn motor_captor_vert (Motor.speed ~tach_limit:
-                     (abs(m -angle_v)) (fst speed_down.(diff_line).(diff_col)))
-         |None -> assert false);
-
-
-        if (diff_col >= 0) then (*on va vers la gauche*)
-          (
-            (match (Robot.read meas_right) with
-             |Some m  ->
-                Motor.set C.conn motor_captor_r (Motor.speed ~tach_limit:
-                   (abs(angle_r - m)) (scd speed_up.(diff_line).(diff_col)))
-             |None -> assert false);
-
-            (match (Robot.read meas_left) with
-             |Some m  ->
-                Motor.set C.conn motor_captor_l (Motor.speed ~tach_limit:
-                      (abs(m - angle_l + 5)) (lst speed_up.(diff_line).(diff_col)))
-             |None -> assert false);
-
-            wait_down_left angle_l f
-          )
-        else (*on va vers la droite*)
-          (
-            (match (Robot.read meas_right) with
-             |Some m  ->
-                Motor.set C.conn motor_captor_r (Motor.speed
-                   ~tach_limit: (abs(m - angle_r + 5))
-                   ((-1)*(lst speed_up.(diff_line).(diff_col))))
-             |None -> assert false);
-
-            (match (Robot.read meas_left) with
-             |Some m  ->
-                Motor.set C.conn motor_captor_l (Motor.speed
-                      ~tach_limit: (abs(angle_l - m))
-                      ((-1)*(scd speed_up.(diff_line).(diff_col))))
-             |None -> assert false);
-
-            wait_down_right angle_r f
-          )
-      )
-
 
 
 
@@ -475,7 +499,15 @@ struct
 
 
   let run () =
-    scan_game stop ();(*petit test pr direct rescanner le nv jeu*) 
+    (* scan_game stop (); *)
+
+    current_col := 1;
+    current_line := 5;
+
+    (* scan_case 5 1 stop; *)
+    scan_case 0 0 stop;
+
+    (* adjustment 0 0 stop; *)
     Robot.run r
 
 end
