c819049796503eecef9370b05549f0c1d65a2246
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-03-23 23:12:24 +0100

    Replace the obsolete scanPiece by the one that performs diagonal moves.

--- scanPiece.mli
+++ scanPiece.mli
@@ -1,18 +1,17 @@
 module Run(C: sig val conn_scan : Mindstorm.bluetooth Mindstorm.conn
                   val r : Robot.t end) :
 sig
-  val scan : int -> (int -> unit) -> unit
-    (**[scan col_new_piece next] scanne les cases de la grille où l'autre
-       joueur peut mettre sa pièce jusqu'à ce qu'il trouve une nouvelle pièce.
-       [col_new_piece] est la colonne où le robot à mis sa pièce (utile pr
-       mettre à jour le jeu courant).
-       [next] est la fct qui va être lancée lorsque le robot a trouvé une
-       nouvelle pièce ds le jeu et elle est appliquée sur la colonne où la
-       pièce a été ajoutée (utile pr pouvoir ensuite calculer le coup prochain
-    *)
+  val add_piece : int -> unit
+    (** [add_piece col] add a piece (typically because the computer
+        played there) so that the scan knows where to look. *)
 
-  val run : unit -> unit
-    (** Starts the event loop associated with the scan. *)
+  val scan : (int -> unit) -> unit
+    (** [scan k] scanne les cases de la grille où l'autre joueur peut
+        mettre sa pièce jusqu'à ce qu'il trouve une nouvelle pièce.
+        [k] est la fct qui va être lancée lorsque le robot a trouvé
+        une nouvelle pièce ds le jeu et elle est appliquée sur la
+        colonne où la pièce a été ajoutée (utile pr pouvoir ensuite
+        calculer le coup prochain. *)
 
   val stop : int -> unit
     (** Stop all motors associated with the scan. *)
