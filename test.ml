de0e401b6794af14ca83ad1966f5574d3c4813fe
Sbabo David <davidsbabo315@msn.com>
2010-06-21 14:29:35 +0200

    Copie du push de Velena

--- test.ml
+++ test.ml
@@ -2,13 +2,13 @@ let () =
 for j=0 to 6 do
 let g = Structure.create_game() in
   Structure.initboard g;
-  Structure.makemove g j;
+  ignore(Structure.makemove g j);
   Printf.printf "J = %d\n%!" j;
   let i = ref 2 in
   while Structure.get_game_result g = -1 do   
     let x = Ia.move_for g in
     Printf.printf "Move %d : %d\n%!" !i x;
-    Structure.makemove g x;
+   ignore(Structure.makemove g x);
     i := !i + 1;
   done;
   Printf.printf "Gagné par J%d\n\n\n%!" (Structure.get_game_result g)
