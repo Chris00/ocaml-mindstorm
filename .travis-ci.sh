a01d7ec6232cceaf91966db25da164ba3b309a8e
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2017-02-03 22:49:56 +0100

    Add Travis folds

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,7 @@
 ## Based on https://github.com/ocaml/ocaml-travisci-skeleton
 
+echo -en "travis_fold:start:prepare.ci\r"
+
 # Basic OCaml and opam installation
 fork_user=ocaml
 fork_branch=master
@@ -17,6 +19,8 @@ export OPAMYES=1
 eval $(opam config env)
 
 opam install ocamlfind oasis cppo lwt
+
+echo -en "travis_fold:end:prepare.ci\r"
 
 # See https://github.com/ocaml/oasis/issues/94
 oasis setup
