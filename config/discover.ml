50e8190955ca62e4631ad61385077aa9367361ef
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2018-02-04 19:42:07 +0100

    Use pkg-config for library detection when possible
    Also support systems mingw64 & win64.

--- config/discover.ml
+++ config/discover.ml
@@ -1,44 +1,55 @@
 open Stdio
 open Printf
 module C = Configurator
+module P = Configurator.Pkg_config
 
 let split_ws str =
   Base.(String.(split str ~on:' ' |> List.filter ~f:((<>) "")))
 
-(* Naive libusb location detection *)
-let usb_include () =
-  if Caml.Sys.file_exists "/usr/include/libusb-1.0/libusb.h" then
-    "/usr/include/libusb-1.0/"
-  else ""
+let merge_pkg p1 p2 = match p2 with
+  | Some p2 -> { P.cflags = p1.P.cflags @ p2.P.cflags;
+                 P.libs = p1.P.libs @ p2.P.libs }
+  | None -> p1
 
-let get_flags c =
-  let c_flags = if Sys.word_size = 64 then ["-DARCH64"] else [] in
-  let usb_include = usb_include () in
+let get_bluetooth c =
   let sys = C.ocaml_config_var_exn c "system"in
-  if sys = "linux" then
-    if String.(usb_include <> "") then
-      ("-DHAS_USB" :: "-I" :: usb_include :: c_flags,
-       ["-lbluetooth"; "-lusb-1.0"])
-    else (c_flags, ["-lbluetooth"])
-  else if sys = "msvc" then
-    ("/nologo" :: "/DWIN32" :: c_flags,
-     ["/I\"C:\\Program Files\\Microsoft Platform SDK\\Include\""])
+  let ms_sdk = "\"C:\\Program Files\\Microsoft Platform SDK\\Include\"" in
+  if sys = "linux" then (
+    match P.get c with
+    | None -> None
+    | Some p -> P.query p ~package:"bluez"
+  )
+  else if sys = "msvc" || sys = "win64" then
+    Some { P.cflags = ["/nologo"; "/DWIN32"; "/I" ^ ms_sdk];  libs = []}
+  else if sys = "mingw64" then
+    Some { P.cflags = ["-I"; ms_sdk];  libs = [] }
   else if sys = "macosx" then
-    ("-DMACOSX" :: c_flags,
-     ["-I"; "/System/Library/Frameworks/IOBluetooth.framework/Headers"])
+    let mac_bt = "/System/Library/Frameworks/IOBluetooth.framework/Headers" in
+    Some { P.cflags = ["-DMACOSX"; "-I"; mac_bt];  libs = [] }
   else
     C.die "System %S currently not supported.  Please contact the \
            mindstorm developers." sys
 
+let get_usb c =
+  match P.get c with
+  | None -> None
+  | Some p ->
+     match P.query p ~package:"libusb-1.0" with
+     | None -> None
+     | Some p -> Some { p with P.cflags = "-DHAS_USB" :: p.P.cflags }
+
 let discover c =
-  let c_flags, libs = get_flags c in
+  let p = if Sys.word_size = 64 then { P.cflags = ["-DARCH64"]; libs = [] }
+          else { P.cflags = []; libs = [] } in
+  let p = merge_pkg p (get_bluetooth c) in
+  let p = merge_pkg p (get_usb c) in
   let c_flags =
     match Caml.Sys.getenv "MINDSTORM_CFLAGS" with
-    | exception Not_found -> c_flags
+    | exception Not_found -> p.P.cflags
     | alt_cflags -> split_ws alt_cflags in
   let libs =
     match Caml.Sys.getenv "MINDSTORM_LIBS" with
-    | exception Not_found -> libs
+    | exception Not_found -> p.P.libs
     | alt_libs -> "-lm" :: split_ws alt_libs in
 
   let write_sexp file sexp =
@@ -50,7 +61,7 @@ let cppo file c =
   let ocaml_version = C.ocaml_config_var_exn c "version" in
   let system = C.ocaml_config_var_exn c "system" in
   let arch = C.ocaml_config_var_exn c "architecture" in
-  let has_usb = String.(usb_include () <> "") in
+  let has_usb = String.(get_usb c <> None) in
   let cmd = sprintf "cppo -D %s -D %s%s -V OCAML:%s %s"
                        system arch (if has_usb then " -D HAS_USB" else "")
                        ocaml_version (Filename.quote file) in
